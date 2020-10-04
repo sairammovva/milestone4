cb84d2e51076016a53a8bf8d605b5e3637b4c078
sairam <m.sairam14021998@gmail.com>
2020-10-04 09:22:57 +0530

    milestone4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { Task2309Component } from './task2309/task2309.component';
 import { Task2409headComponent } from './task2409head/task2409head.component';
 import { Task2409bodyComponent } from './task2409body/task2409body.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { CrudComponent } from './crud/crud.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { Milestone4Component } from './milestone4/milestone4.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,16 @@ import { CalculatorComponent } from './calculator/calculator.component';
     Task2309Component,
     Task2409headComponent,
     Task2409bodyComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    CrudComponent,
+    SignupFormComponent,
+    Milestone4Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
